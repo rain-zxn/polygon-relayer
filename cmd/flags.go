e896c8b99f97c6bc8f56f4a0b28b75083b135a9c
christianxiao <christian.xiao@outlook.com>
2021-07-23 22:15:39 +0800

    Add nofeemode

--- cmd/flags.go
+++ cmd/flags.go
@@ -29,6 +29,11 @@ var (
 		Usage: "For local test, do not send to outside",
 	}
 
+	NofeemodeFlag = cli.BoolFlag{
+		Name:  "nofee",
+		Usage: "For local test, do not charge fee",
+	}
+
 	LogLevelFlag = cli.UintFlag{
 		Name:  "loglevel",
 		Usage: "Set the log level to `<level>` (0~6). 0:Trace 1:Debug 2:Info 3:Warn 4:Error 5:Fatal 6:MaxLevel",
