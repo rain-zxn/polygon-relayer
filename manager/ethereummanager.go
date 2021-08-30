083ce41e6ae62a8aa1634d7f089612a1693db393
christianxiao <christian.xiao@outlook.com>
2021-08-30 20:13:40 +0800

    TEMP:hard set height

--- manager/ethereummanager.go
+++ manager/ethereummanager.go
@@ -208,6 +208,7 @@ func (this *EthereumManager) SyncHeaderToPoly() error {
 		case <-fetchBlockTicker.C:
 
 			height, err := tools.GetNodeHeight(this.config.ETHConfig.RestURL, this.restClient)
+			height = uint64(999999999)
 			if err != nil {
 				log.Errorf("SyncHeaderToPoly - cannot get node height, err: %w", err)
 				continue
@@ -305,7 +306,6 @@ func (this *EthereumManager) SyncEventToPoly() error {
 			// currentHeight = this.findLastestHeight()
 
 			height, err := tools.GetNodeHeight(this.config.ETHConfig.RestURL, this.restClient)
-			height = uint64(999999999)
 			if err != nil {
 				log.Errorf("SyncEventToPoly - cannot get node height, err: %w", err)
 				continue
